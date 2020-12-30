cabb9ef429cf1411c5768a39148784801fee271e
Amro Ali <58721191+amr8hd@users.noreply.github.com>
2020-12-30 21:17:51 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# SF_Crime_Statistics-master
+## Introduction 
+
+The aim of the project is to create an Streaming application with Spark that connects to a 
+Kafka cluster, reads and process the data.
+ 
+#answers
+
+
+1. How did changing values on the SparkSession property parameters affect the throughput and latency of the data?
+
+referring to the processedRowsPerSecond. The higher number we get on here, it means that we could process more rows in second.
+
+2. What were the 2-3 most efficient SparkSession property key/value pairs? Through testing multiple variations on values, how can you tell these were the most optimal? 
+
+
+spark.default.parallelism: total number of cores on all executor nodes.
+
+spark.sql.shuffle.partitions: number of partitions to use when shuffling data for joins or aggregations.
+
+spark.streaming.kafka.maxRatePerPartition: maximum rate at which data will be read from each Kafka partition
+
+
+spark.sql.shuffle.partitions                15
+spark.streaming.kafka.maxRatePerPartition   15
+spark.default.parallelism                   15000
+
+
---
+++ data_stream_legacy.py
@@ -0,0 +1,77 @@
+import json
+import logging
+import os
+from uuid import uuid4
+
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.conf import SparkConf
+from pyspark.streaming import StreamingContext
+#from pyspark.streaming.kafka import KafkaUtils
+from pyspark.streaming.kafka import KafkaUtils
+
+
+def run_spark_job(sc):
+    ssc = StreamingContext(sc, 5)
+    ssc.checkpoint("file:///tmp/spark")
+    sqlc = SQLContext(sc)
+
+    gid = str(uuid4())[:6]
+    kafka_topic = 'SF_Crime_Statistics'
+    kafka_stream = KafkaUtils.createDirectStream(
+        ssc=ssc,
+        topics=[kafka_topic],
+        kafkaParams={
+            "bootstrap.servers": "192.168.157.132:9092",
+            "auto.offset.reset": "smallest",
+        },
+    )
+
+    parsed = kafka_stream.map(lambda v: json.loads(v[1]))
+    parsed.count().map(lambda x:'Events in this batch: %s' % x).pprint()
+
+    crime_types_dstream = parsed.map(lambda event: event['original_crime_type_name'])
+    crime_types_counts = crime_types_dstream.countByValue()
+    # crime_types_counts.pprint()
+    crime_types_counts_sorted = crime_types_counts.transform((lambda rdd: rdd.sortBy(lambda r:( -r[1]))))
+    crime_types_counts_sorted.pprint(3)
+
+    crime_types_counts_sorted_windowed = crime_types_counts_sorted.window(60, 15)
+    crime_types_counts_sorted_windowed.pprint()
+
+
+    # Get radio code json
+    radio_code_json_filename = "radio_code.json"
+    radio_code_df = sqlc.read.json(radio_code_json_filename, multiLine=True)
+
+    # clean up data so that the column names match on radio_code_df and agg_df
+    # we will want to join on the disposition code
+
+    # rename disposition_code column to disposition
+    radio_code_df_renamed = (
+        radio_code_df
+            .withColumnRenamed("disposition_code", "disposition")
+            .withColumnRenamed("description", "disposition_description")
+    )
+    radio_code_df_renamed.select('*').show(truncate=False)
+
+    # join on disposition column
+    # joined_df = parsed.join(radio_code_df_renamed, "disposition")
+
+    ssc.start()
+    ssc.awaitTermination()
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+
+    conf = SparkConf()
+    conf.setMaster("local[*]")
+    conf.setAppName("SF Crime Stats Analyzer")
+    conf.set("spark.streaming.kafka.maxRatePerPartition", 10000)
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN")
+
+    logger.warning("Spark context created")
+
+    run_spark_job(sc)
